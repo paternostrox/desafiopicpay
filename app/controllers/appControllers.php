dd5287ea316557b71747697dba93bf15d8927714
laisamartins <laisamscc@gmail.com>
2019-06-09 20:02:18 -0300

    corrigindo um errinho

--- app/controllers/appControllers.php
+++ app/controllers/appControllers.php
@@ -81,27 +81,27 @@ $app->get('/users/username', function() use ($app) {
      
     
     # paginacao
-    $firstPage = "<a href='http://localhost/pp/users/name?n=$name&page=1'>First page</a> ";
-    $lastPage = "<a href='http://localhost/pp/users/name?n=$name&page=$totalPages'>Last page ($totalPages)</a> ";;
+    $firstPage = "<a href='http://localhost/pp/users/name?n=$username&page=1'>First page</a> ";
+    $lastPage = "<a href='http://localhost/pp/users/name?n=$username&page=$totalPages'>Last page ($totalPages)</a> ";;
     echo $firstPage, "\n";
 
     # pagina possui anterior
     if($page > 1) { 
       $toDisplay = $page - 1;
-      $previousPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Previous page ($toDisplay)</a> ";
+      $previousPage = "<a href='http://localhost/pp/users/name?n=$username&page=$toDisplay'>Previous page ($toDisplay)</a> ";
 
       echo $previousPage, "\n";
       # pagina possui anterior e proximo
       if($page < $totalPages) { 
         $toDisplay = $page + 1;
-        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Next page ($toDisplay)</a> ";
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$username&page=$toDisplay'>Next page ($toDisplay)</a> ";
       echo $nextPage, "\n";
       }
 
       # pagina possui apenas proximo
     } else if ($page < $totalPages){ 
         $toDisplay = $page + 1;
-        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Page $toDisplay</a> "; 
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$username&page=$toDisplay'>Page $toDisplay</a> "; 
         echo $nextPage, "\n";
       }
     echo $lastPage, "\n";
