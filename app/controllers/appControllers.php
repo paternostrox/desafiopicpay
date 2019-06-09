d3772dd37a485b6df82b300c87216d7cb1b032c6
laisamartins <laisamscc@gmail.com>
2019-06-09 18:58:43 -0300

    Update appControllers.php
    paginação adicionada

--- app/controllers/appControllers.php
+++ app/controllers/appControllers.php
@@ -2,46 +2,114 @@
 
 # === api
 # ==================================================
-use Illuminate\Support\Facades\DB;
 
-
-
+# busca usuarios em nome
 $app->get('/users/name', function() use ($app) {
 
   $name = $app->request->get('n');
   if ( $name ) {
     $results = [];
+
+    $toDisplay = 0;
+    $perPage = 15;
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    $startAt = $perPage * ($page - 1);
+
+    $count = Users::where('name','LIKE',"%{$name}%")->count();
+    $totalPages = ceil($count / $perPage);
+
+    # execucao da busca
     $results = Users::where('name','LIKE',"%{$name}%")
     ->orderByRaw("FIELD(id, (SELECT id FROM relevancia1 WHERE id = users.id))")
     ->orderByRaw("FIELD(id, (SELECT id FROM relevancia2 WHERE id = users.id))")
-    ->orderBy('name')
+    ->orderByRaw("name LIMIT ?, ?", [$startAt, $perPage])
     ->get();
-    #->paginate(15);
+  
     
-    $message = $results->count() . ' results';
+    # paginacao
+    $firstPage = "<a href='http://localhost/pp/users/name?n=$name&page=1'>First page</a> ";
+    $lastPage = "<a href='http://localhost/pp/users/name?n=$name&page=$totalPages'>Last page ($totalPages)</a> ";;
+    echo $firstPage, "\n";
+
+    # pagina possui anterior
+    if($page > 1) { 
+      $toDisplay = $page - 1;
+      $previousPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Previous page ($toDisplay)</a> ";
+
+      echo $previousPage, "\n";
+      # pagina possui anterior e proximo
+      if($page < $totalPages) { 
+        $toDisplay = $page + 1;
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Next page ($toDisplay)</a> ";
+      echo $nextPage, "\n";
+      }
+
+      # pagina possui apenas proximo
+    } else if ($page < $totalPages){ 
+        $toDisplay = $page + 1;
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Page $toDisplay</a> "; 
+        echo $nextPage, "\n";
+      }
+    echo $lastPage, "\n";
+
+    $message = $count . ' results';
     return helpers::jsonResponse(false, $message, $results );
   }
 
 });
 
+# busca usuarios em username
 $app->get('/users/username', function() use ($app) {
 
   $username = $app->request->get('u');
   if ( $username ) {
     $results = [];
+    $toDisplay = 0;
+    $perPage = 15;
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    $startAt = $perPage * ($page - 1);
+
+    $count = Users::where('username','LIKE',"%{$username}%")->count();
+    $totalPages = ceil($count / $perPage);
+
+    # execucao da busca
     $results = Users::where('username','LIKE',"%{$username}%")
     ->orderByRaw("FIELD(id, (SELECT id FROM relevancia1 WHERE id = users.id))")
     ->orderByRaw("FIELD(id, (SELECT id FROM relevancia2 WHERE id = users.id))")
-    ->orderBy('name')
+    ->orderByRaw("name LIMIT ?, ?", [$startAt, $perPage])
     ->get();
-    #->paginate(15);
+     
+    
+    # paginacao
+    $firstPage = "<a href='http://localhost/pp/users/name?n=$name&page=1'>First page</a> ";
+    $lastPage = "<a href='http://localhost/pp/users/name?n=$name&page=$totalPages'>Last page ($totalPages)</a> ";;
+    echo $firstPage, "\n";
 
-    $message = $results->count() . ' results';
+    # pagina possui anterior
+    if($page > 1) { 
+      $toDisplay = $page - 1;
+      $previousPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Previous page ($toDisplay)</a> ";
+
+      echo $previousPage, "\n";
+      # pagina possui anterior e proximo
+      if($page < $totalPages) { 
+        $toDisplay = $page + 1;
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Next page ($toDisplay)</a> ";
+      echo $nextPage, "\n";
+      }
+
+      # pagina possui apenas proximo
+    } else if ($page < $totalPages){ 
+        $toDisplay = $page + 1;
+        $nextPage = "<a href='http://localhost/pp/users/name?n=$name&page=$toDisplay'>Page $toDisplay</a> "; 
+        echo $nextPage, "\n";
+      }
+    echo $lastPage, "\n";
+
+    $message = $count . ' results';
     return helpers::jsonResponse(false, $message, $results );
   }
 
 });
 
 $app->get('/', function() use ($app) {});
-
-
